3b5aa13f895d0af62bdb26254254807fee849319
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-06-30 12:46:14 +0900

    키패드 누르기

--- solution.cpp
+++ solution.cpp
@@ -1,62 +1,57 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-int solution(string s) {
-    int answer = 0;
-    string num="";
-    for(int i=0; i<s.size(); i++){
-        if(s[i]>='0' && s[i]<='9') num+=s[i];
+string solution(vector<int> numbers, string hand) {
+    string answer = "";
+    bool right=false;
+    if(hand=="right") right=true;
+    
+    int pad[4][3]={{1,2,3},
+                   {4,5,6},
+                   {7,8,9},
+                   {'*',0,'#'}};
+    pair<int, int> L={3,0};
+    pair<int, int> R={3,2};
+    
+    for(int i=0; i<numbers.size(); i++){
+        pair<int, int> n;
+        for(int j=0; j<4; j++)
+            for(int k=0; k<3; k++)
+                if(numbers[i]==pad[j][k]) n={j, k};
+        
+        if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7){
+            L=n;
+            answer+="L";
+        }
+        else if(numbers[i]==3 || numbers[i]==6 || numbers[i]==9){
+            R=n;
+            answer+="R";
+        }
         else{
-            if(s[i]=='z'){ //0
-                num+='0';
-                i+=3;
+            int l_dis=abs(L.first-n.first)+abs(L.second-n.second);
+            int r_dis=abs(R.first-n.first)+abs(R.second-n.second);
+            if(l_dis<r_dis){
+                L=n;
+                answer+="L";
             }
-            else if(s[i]=='o'){ //1
-                num+='1';
-                i+=2;
+            else if(l_dis>r_dis){
+                R=n;
+                answer+="R";
             }
-            else if(s[i]=='t'){ //2, 3
-                if(s[i+1]=='w'){
-                    num+='2';
-                    i+=2;
+            else if(l_dis==r_dis){
+                if(right==true){
+                    R=n;
+                    answer+="R";
                 }
-                else if(s[i+1]=='h'){
-                    num+='3';
-                    i+=4;
+                else if(right==false){
+                    L=n;
+                    answer+="L";
                 }
-            }
-            else if(s[i]=='f'){ //4, 5
-                if(s[i+1]=='o'){
-                    num+='4';
-                    i+=3;
-                }
-                else if(s[i+1]=='i'){
-                    num+='5';
-                    i+=3;
-                }
-            }
-            else if(s[i]=='s'){ //6, 7
-               if(s[i+1]=='i'){
-                    num+='6';
-                    i+=2;
-                }
-                else if(s[i+1]=='e'){
-                    num+='7';
-                    i+=4;
-                }
-            }
-            else if(s[i]=='e'){ //8
-                num+='8';
-                i+=4;
-            }
-            else if(s[i]=='n'){ //9
-                num+='9';
-                i+=3;
             }
         }
     }
-    answer=stoi(num);
     return answer;
 }
