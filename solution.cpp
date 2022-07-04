0cfc30482cad5c8343b74409f25c2a21a569dd6c
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-07-04 13:17:50 +0900

    크레인 인형뽑기 게임

--- solution.cpp
+++ solution.cpp
@@ -1,57 +1,36 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
-string solution(vector<int> numbers, string hand) {
-    string answer = "";
-    bool right=false;
-    if(hand=="right") right=true;
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    stack<int> s; //바구니
     
-    int pad[4][3]={{1,2,3},
-                   {4,5,6},
-                   {7,8,9},
-                   {'*',0,'#'}};
-    pair<int, int> L={3,0};
-    pair<int, int> R={3,2};
-    
-    for(int i=0; i<numbers.size(); i++){
-        pair<int, int> n;
-        for(int j=0; j<4; j++)
-            for(int k=0; k<3; k++)
-                if(numbers[i]==pad[j][k]) n={j, k};
+    for(int i=0; i<moves.size(); i++){
+        int grap=moves[i]-1; //뽑는 열
+        if(board[board.size()-1][grap]==0) continue;
         
-        if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7){
-            L=n;
-            answer+="L";
+        int doll;
+        for(int j=0; j<board.size(); j++){
+            if(board[j][grap]!=0){
+                doll=board[j][grap];
+                board[j][grap]=0;
+                break;
+            }
         }
-        else if(numbers[i]==3 || numbers[i]==6 || numbers[i]==9){
-            R=n;
-            answer+="R";
-        }
+        
+        if(s.size()==0) s.push(doll);
         else{
-            int l_dis=abs(L.first-n.first)+abs(L.second-n.second);
-            int r_dis=abs(R.first-n.first)+abs(R.second-n.second);
-            if(l_dis<r_dis){
-                L=n;
-                answer+="L";
+            if(s.top()==doll){
+                s.pop();
+                answer+=2;
             }
-            else if(l_dis>r_dis){
-                R=n;
-                answer+="R";
-            }
-            else if(l_dis==r_dis){
-                if(right==true){
-                    R=n;
-                    answer+="R";
-                }
-                else if(right==false){
-                    L=n;
-                    answer+="L";
-                }
-            }
+            else s.push(doll);
         }
     }
+    
     return answer;
 }
