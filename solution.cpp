08480f92ffc5e536ae41d61ef89dcf64a9fbbbbb
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-07-08 10:25:08 +0900

    음양 더하기

--- solution.cpp
+++ solution.cpp
@@ -1,17 +1,13 @@
 #include <string>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
-int solution(vector<int> numbers) {
-    int sum=0;
-    bool arr[10];
-    memset(arr, false, sizeof(arr));
-    for(int i=0; i<10; i++)
-        for(int j=0; j<numbers.size(); j++)
-            if(numbers[j]==i) arr[i]=true;
-    for(int i=0; i<10; i++)
-        if(arr[i]==false) sum+=i;
-    return sum;
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for(int i=0; i<absolutes.size(); i++){
+        if(signs[i]==true) answer+=absolutes[i];
+        else answer-=absolutes[i];
+    }
+    return answer;
 }
