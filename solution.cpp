b7a13bf5e67f7481711b0f97539cc27734dded09
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-02 13:19:24 +0900

    타겟 넘버

--- solution.cpp
+++ solution.cpp
@@ -1,39 +1,22 @@
 #include <string>
-#include <unordered_set>
-#include <algorithm>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
-int solution(int N, int number) {
-    int answer = -1;
-    
-    unordered_set<int> s[8];
-    
-    int sum=0;
-    for(int i=0; i<8; i++){
-        sum=sum*10+N;
-        s[i].insert(sum);
+int answer;
+
+void dfs(vector<int> numbers, int target, int sum, int cnt){
+    if(cnt==numbers.size()){
+        if(sum==target) answer++;
+        return;
     }
-    
-    for(int i=1; i<8; i++){
-        for(int j=0; j<i; j++){
-            for(int a : s[j]){
-                for(int b : s[i-j-1]){
-                    s[i].insert(a+b);
-                    s[i].insert(a-b);
-                    s[i].insert(a*b);
-                    if(b!=0) s[i].insert(a/b);
-                }
-            }
-        }
-    }
-    for(int i=0; i<8; i++){
-        if(s[i].find(number)!=s[i].end()){
-            answer=i+1;
-            break;
-        }
-    }
+    dfs(numbers, target, sum+numbers[cnt], cnt+1);
+    dfs(numbers, target, sum-numbers[cnt], cnt+1);
+}
+
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, 0);
     
     return answer;
 }
