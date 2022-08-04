d8179ff41dd6ebda725dfafef8ab0eb91854b4b0
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-04 15:38:41 +0900

    입국심사

--- solution.cpp
+++ solution.cpp
@@ -1,22 +1,29 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int answer;
-
-void dfs(vector<int> numbers, int target, int sum, int cnt){
-    if(cnt==numbers.size()){
-        if(sum==target) answer++;
-        return;
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    
+    sort(times.begin(), times.end());
+    long long MIN=1;
+    long long MAX=(long long) times[times.size()-1]*n;
+    
+    while(MIN<=MAX){
+        long long MID=(MIN+MAX)/2;
+        long long cnt=0;
+        
+        for(int i=0; i<times.size(); i++)
+            cnt+=MID/times[i];
+        
+        if(cnt<n) MIN=MID+1;
+        else{
+            if(MID<=MAX) answer=MID;
+            MAX=MID-1;
+        }
     }
-    dfs(numbers, target, sum+numbers[cnt], cnt+1);
-    dfs(numbers, target, sum-numbers[cnt], cnt+1);
-}
-
-int solution(vector<int> numbers, int target) {
-    dfs(numbers, target, 0, 0);
     
     return answer;
 }
