6ccb84d5f640ce6d1b770e79c9f00f763f0dd559
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-05 11:25:42 +0900

    가장 먼 노드

--- solution.cpp
+++ solution.cpp
@@ -1,29 +1,53 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
-long long solution(int n, vector<int> times) {
-    long long answer = 0;
+int bfs(int n, vector<int> v[]){
+    queue<pair<int, int>> q; //노드번호, 거리
+    bool visit[20001]={false};
+    int dis[20001];
     
-    sort(times.begin(), times.end());
-    long long MIN=1;
-    long long MAX=(long long) times[times.size()-1]*n;
+    for(int i=1; i<n+1; i++) dis[i]=2e9;
+    dis[1]=0;
+    visit[1]=true;
+    q.push({1, 0});
     
-    while(MIN<=MAX){
-        long long MID=(MIN+MAX)/2;
-        long long cnt=0;
+    while(!q.empty()){
+        int cur_num=q.front().first;
+        int cur_dis=q.front().second;
+        q.pop();
         
-        for(int i=0; i<times.size(); i++)
-            cnt+=MID/times[i];
-        
-        if(cnt<n) MIN=MID+1;
-        else{
-            if(MID<=MAX) answer=MID;
-            MAX=MID-1;
+        for(int i=0; i<v[cur_num].size(); i++){
+            int next=v[cur_num][i];
+            if(visit[next]==false || dis[next]>cur_dis+1){ //거리비교에서 = 빼야됨
+                visit[next]=true;
+                dis[next]=cur_dis+1;
+                q.push({next, cur_dis+1});
+            }
         }
     }
+    int MAX=dis[2];
+    for(int i=3; i<n+1; i++) MAX=max(MAX, dis[i]);
+    int sol=0;
+    for(int i=2; i<n+1; i++)
+        if(dis[i]==MAX) sol++;
+    return sol;
+}
+
+int solution(int n, vector<vector<int>> edge) {
+    int answer = 0;
+    vector<int> v[20001];
+    for(int i=0; i<edge.size(); i++){
+        int a=edge[i][0];
+        int b=edge[i][1];
+        v[a].push_back(b);
+        v[b].push_back(a);
+        
+    }
+    answer=bfs(n, v);
     
     return answer;
 }
