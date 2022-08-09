4dec7c1a68a92ba0aadf3b60d99a657ee673be8d
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-09 10:31:28 +0900

    행렬 테두리 회전하기

--- solution.cpp
+++ solution.cpp
@@ -1,20 +1,48 @@
-#include <iostream>
 #include <string>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
-int solution(string s)
-{
-    int answer = -1;
-
-    stack<char> stk;
-    stk.push(s[0]);
-    for(int i=1; i<s.size(); i++){
-        if(stk.empty() || s[i]!=stk.top()) stk.push(s[i]);
-        else if(s[i]==stk.top()) stk.pop();
+int solve(int rows, int columns, vector<int> queries, int arr[100][100]){
+    pair<int, int> lu={queries[0]-1, queries[1]-1}; //행, 열
+    pair<int, int> rd={queries[2]-1, queries[3]-1};
+    int temp=arr[lu.first][lu.second];
+    int MIN=temp;
+    
+    for(int i=lu.first; i<rd.first; i++){ //좌
+        MIN=min(MIN, arr[i][lu.second]);
+        arr[i][lu.second]=arr[i+1][lu.second];
     }
-    if(stk.empty()) answer=1;
-    else answer=0;
+    
+    for(int i=lu.second; i<rd.second; i++){ //하
+        MIN=min(MIN, arr[rd.first][i]);
+        arr[rd.first][i]=arr[rd.first][i+1];
+    }
+    
+    for(int i=rd.first; i>lu.first; i--){ //우
+        MIN=min(MIN, arr[i][rd.second]);
+        arr[i][rd.second]=arr[i-1][rd.second];
+    }
+    
+    for(int i=rd.second; i>lu.second; i--){ //상
+        MIN=min(MIN, arr[lu.first][i]);
+        arr[lu.first][i]=arr[lu.first][i-1];
+    }
+    
+    arr[lu.first][lu.second+1]=temp;
+        
+    return MIN;
+}
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+    int arr[100][100];
+    int num=1;
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<columns; j++)
+            arr[i][j]=num++;
+    for(int i=0; i<queries.size(); i++)
+        answer.push_back(solve(rows, columns, queries[i], arr));
+    
     return answer;
 }
