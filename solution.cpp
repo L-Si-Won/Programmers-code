2627b038ca449caa31d5615d51dde539a05939f1
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-16 15:32:50 +0900

    거리두기 확인하기

--- solution.cpp
+++ solution.cpp
@@ -1,48 +1,51 @@
 #include <string>
-#include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-int solution(string str1, string str2) {
-    //소문자로 통일
-    for(int i=0; i<str1.size(); i++)
-        if(str1[i]>='A' && str1[i]<='Z') str1[i]+=32;
-    for(int i=0; i<str2.size(); i++)
-        if(str2[i]>='A' && str2[i]<='Z') str2[i]+=32;
+int dx[]={1, 0, -1, 0};
+int dy[]={0, 1, 0, -1};
+bool visit[5][5];
+
+bool dfs(int x, int y, vector<string> v, int cnt){
+    if(cnt==3) return true;
     
-    //문자 나누기
-    unordered_set<string> str;
-    unordered_map<string, int> m1;
-    unordered_map<string, int> m2;
+    if(cnt>0 && v[x][y]=='P') return false;
     
-    for(int i=0; i<str1.size()-1; i++){
-        if((str1[i]>='a' && str1[i]<='z') && (str1[i+1]>='a' && str1[i+1]<='z')){
-            string temp=str1.substr(i, 2);
-            m1[temp]++;
-            str.insert(temp);
+    for(int i=0; i<4; i++){
+        int nx=x+dx[i];
+        int ny=y+dy[i];
+        if(nx>=0 && ny>=0 && nx<5 && ny<5){
+            if(visit[nx][ny]==false){
+                if(v[nx][ny]!='X'){
+                    visit[nx][ny]=true;
+                    if(dfs(nx, ny, v, cnt+1)==false) return false;
+                    visit[nx][ny]=false;
+                }
+            }
         }
     }
-    for(int i=0; i<str2.size()-1; i++){
-        if((str2[i]>='a' && str2[i]<='z') && (str2[i+1]>='a' && str2[i+1]<='z')){
-            string temp=str2.substr(i, 2);
-            m2[temp]++;
-            str.insert(temp);
+    return true;
+}
+
+int solve(vector<string> v){
+    memset(visit, false, sizeof(visit));
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(v[i][j]=='P'){
+                visit[i][j]=true;
+                if(dfs(i, j, v, 0)==false) return 0;
+                visit[i][j]=false;
+            }
         }
     }
-    
-    //자카드 유사도 계산
-    int sum=0; //합집합 크기
-    int same=0; //교집합 크기
-    for(auto iter:str){
-        sum+=max(m1[iter], m2[iter]);
-        same+=min(m1[iter], m2[iter]);
-    }
-    
-    if(sum==0 && same==0) return 65536;
-    
-    return 65536*same/sum;
+    return 1;
+}
+
+vector<int> solution(vector<vector<string>> places) {
+    vector<int> answer;
+    for(int i=0; i<5; i++)
+        answer.push_back(solve(places[i]));
+    return answer;
 }
