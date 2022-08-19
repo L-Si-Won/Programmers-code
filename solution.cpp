a2fb8c0f1583f2acafa2bb686e13ba0c9187c1f6
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-19 11:02:32 +0900

    튜플

--- solution.cpp
+++ solution.cpp
@@ -1,56 +1,34 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
-long long solution(string s) {
-    long long answer = 0;
-    vector<char> rator; //연산자
-    vector<long long> rand; //피연산자
+bool comp(pair<int, int> a, pair<int, int> b){
+    return a.second > b.second ? true : false;
+}
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    map<int, int> m;
     
-    //연산자, 피연산자 구분하기
     string temp="";
     for(int i=0; i<s.size(); i++){
-        if(s[i]>='0' && s[i]<='9') temp+=s[i];
-        else{
-            rand.push_back(stoll(temp));
-            rator.push_back(s[i]);
-            temp="";
-        }
-    }
-    rand.push_back(stoll(temp));
-    
-    //최대값 계산
-    vector<char> v={'*', '+', '-'};
-    do{
-        vector<char> temp_rator=rator;
-        vector<long long> temp_rand=rand;
-        
-        for(int i=0; i<v.size(); i++){
-            for(int j=0; j<temp_rator.size();){
-                if(v[i]==temp_rator[j]){
-                    //해당 연산자 계산
-                    long long a;
-                    if(v[i]=='*') a=temp_rand[j]*temp_rand[j+1];
-                    else if(v[i]=='+') a=temp_rand[j]+temp_rand[j+1];
-                    else if(v[i]=='-') a=temp_rand[j]-temp_rand[j+1];
-                    
-                    //피연산자 2개 삭제
-                    temp_rand.erase(temp_rand.begin()+j);
-                    temp_rand.erase(temp_rand.begin()+j);
-                    //연산결과 삽입
-                    temp_rand.insert(temp_rand.begin()+j, a);
-                    //해당 연산자 삭제
-                    temp_rator.erase(temp_rator.begin()+j);
-                }
-                else j++;
+        if(s[i]>='0' && s[i]<='9'){
+            temp+=s[i];
+            if(s[i+1]==',' || s[i+1]=='}'){
+                m[stoi(temp)]++;
+                temp="";
             }
         }
-        
-        answer=max(answer, abs(temp_rand[0]));
-    }while(next_permutation(v.begin(), v.end()));
+    }
     
+    vector<pair<int, int>> v(m.begin(), m.end());
+    sort(v.begin(), v.end(), comp);
+    
+    for(int i=0; i<v.size(); i++)
+        answer.push_back(v[i].first);
     return answer;
 }
