1c3d9e7bf710c31c7e337bdda4413fe764ecf4a4
L-Si-Won <102215467+L-Si-Won@users.noreply.github.com>
2022-08-20 15:08:34 +0900

    두 큐 합 같게 만들기

--- solution.cpp
+++ solution.cpp
@@ -1,34 +1,54 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <map>
-#include <algorithm>
 
 using namespace std;
 
-bool comp(pair<int, int> a, pair<int, int> b){
-    return a.second > b.second ? true : false;
-}
-
-vector<int> solution(string s) {
-    vector<int> answer;
-    map<int, int> m;
+int solution(vector<int> queue1, vector<int> queue2) {
+    int answer=0;
+    vector<int>temp_queue1=queue1;
+    vector<int>temp_queue2=queue2;
     
-    string temp="";
-    for(int i=0; i<s.size(); i++){
-        if(s[i]>='0' && s[i]<='9'){
-            temp+=s[i];
-            if(s[i+1]==',' || s[i+1]=='}'){
-                m[stoi(temp)]++;
-                temp="";
-            }
-        }
+    //각 큐의 합
+    long long sum1=0, sum2=0;
+    for(int i=0; i<queue1.size(); i++)
+        sum1+=queue1[i];
+    for(int i=0; i<queue2.size(); i++)
+        sum2+=queue2[i];
+    
+    //모든 수의 합이 홀수이면
+    if((sum1+sum2)&2!=0){
+        answer=-1;
+        return answer;
     }
     
-    vector<pair<int, int>> v(m.begin(), m.end());
-    sort(v.begin(), v.end(), comp);
+    int size1=queue1.size();
     
-    for(int i=0; i<v.size(); i++)
-        answer.push_back(v[i].first);
+    int pop1=0, pop2=0; //각 큐에서 pop한 횟수
+    
+    while(1){
+        if(sum1==sum2) break;
+        else if(sum1>sum2){
+            sum1-=queue1[pop1];
+            sum2+=queue1[pop1];
+            queue2.push_back(queue1[pop1]);
+            queue1[pop1]=0;
+            pop1++;
+        }
+        else if(sum1<sum2){
+            sum1+=queue2[pop2];
+            sum2-=queue2[pop2];
+            queue1.push_back(queue2[pop2]);
+            queue2[pop2]=0;
+            pop2++;
+        }
+        
+        if(queue1.size()>size1*3){
+            answer=-1;
+            break;
+        }
+        
+        answer++;
+    }
+    
     return answer;
 }
